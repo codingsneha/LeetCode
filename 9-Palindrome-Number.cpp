b43c697b859bfff19f00ba857a2a19447fb2583d
Sneha Supriya <codingsneha@gmail.com>
2022-10-02 16:13:01 +0530

    add files

--- 9-Palindrome-Number.cpp
+++ 9-Palindrome-Number.cpp
@@ -1,9 +1,25 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
+        long int rev = 0;
+        for (int y = x; y > 0; y /= 10)
+            rev = (rev * 10) + (y % 10);
+        return rev == x;
+    }
+};
+
+
+/*
+SOLUTION USING STRINGS
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
         string s = to_string(x);
         string f = s;
         reverse (s.begin(), s.end());
         return s == f;
     }
 };
+
+*/
