af865dd679852f8b886e47fc1acb540bcc51ee5d
Sneha Supriya <codingsneha@gmail.com>
2022-10-06 02:10:53 +0530

    add files

---
+++ 118-Pascal's-Triangle.cpp
@@ -0,0 +1,25 @@
+/* Approach 1 - Dynamic Programming
+O (n^2)
+*/
+
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> res(numRows);
+        for (int i = 0; i < numRows; i++) {
+            res[i].resize(i + 1);
+            res[i][0] = res[i][i] = 1;
+            for (int j = 1; j < i; j++)
+                res[i][j] = res[i-1][j-1] + res[i-1][j];
+        }  
+        return res;
+    }
+};
+
+
+
+
+
+/* Approach 2 - Binomial theorem
+O (n^2)
+*/
