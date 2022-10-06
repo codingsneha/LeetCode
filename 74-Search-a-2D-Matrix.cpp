d1b2af7fe16efc15f2a404832b2034327059a612
Sneha Supriya <codingsneha@gmail.com>
2022-10-06 17:35:04 +0530

    add files

---
+++ 74-Search-a-2D-Matrix.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& m, int t) {
+        int i = 0, j = m[0].size()-1;
+        
+        while(i<m.size() && j >= 0){
+            if(m[i][j] == t)
+                return true;
+            if(m[i][j] > t)
+                j--;
+            else
+                i++;
+        }
+        
+        return false;
+    }
+};
